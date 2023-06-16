682b7f1416d0d6c0adabcbb92c2a50787023c425
Alice <helena.wu4@gmail.com>
2023-06-16 02:22:32 -0700

    added command to update data stored in JSON from api using fetch and have it load at client start

---
+++ src/commands/ei/updateWhois.js
@@ -0,0 +1,48 @@
+// const { REST } = require("@discordjs/rest");
+// const { Routes } = require("discord-api-types/v9");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const fs = require("fs");
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+
+/**
+ *  Template
+ */
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("updatewhois")
+    .setDescription("Updates the list of users currently in the group."),
+
+  async execute(interaction, client) {
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++")
+    console.log("Updating user data...")
+    console.log(`Command run by ${interaction.user.tag} at: ${new Date().toLocaleString()}`);
+
+    // Replace 'YOUR_DISCORD_USER_ID' with your own Discord user ID
+    const ownerId = process.env.OWNER_ID;
+
+    if (interaction.user.id !== ownerId) {
+      return interaction.reply({
+        content: "This command can only be executed by Alice!",
+        ephemeral: false, 
+      });
+    }
+
+    try {
+      // Fetch data from the API endpoint
+      const response = await fetch("https://eiapi-production.up.railway.app/allMaj");
+      const data = await response.json();
+      fs.writeFileSync("src/data/maj.json", JSON.stringify(data));
+
+      await interaction.reply("User data updated successfully!");
+
+    } catch (error) {
+      console.log(error);
+
+      interaction.reply({
+        content: `Something went wrong.`,
+      });
+    }
+  },
+};
--- src/utils/majLookup.js
+++ src/utils/majLookup.js
@@ -1,19 +1,56 @@
 const fs = require("fs");
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 let majMemberlist;
 
-function loadMajMemberlist() {
+/**
+ *
+ * This function loads the userdata from JSON to the majMemberlist variable.
+ *
+ *************************************************/
+// function loadMajMemberlist() {
+//   if (!majMemberlist) {
+//     const data = fs.readFileSync("src/data/maj.json", "utf8");
+//     majMemberlist = JSON.parse(data);
+
+//     console.log("Loaded Majeggstics Member list");
+//     console.log(majMemberlist)
+//   }
+// }
+
+/**
+ *
+ * Auto updates the Majeggstics Member list with the latest information from the API endpoint.
+ *
+ *****************************/
+
+async function loadMajMemberlist() {
   if (!majMemberlist) {
     const data = fs.readFileSync("src/data/maj.json", "utf8");
     majMemberlist = JSON.parse(data);
 
     console.log("Loaded Majeggstics Member list");
-    console.log(majMemberlist)
+    console.log(majMemberlist);
+  }
+
+  try {
+    const response = await fetch(
+      "https://eiapi-production.up.railway.app/allMaj"
+    );
+    const newData = await response.json();
+
+    // Update the member list with the retrieved information
+    majMemberlist = newData;
+
+    console.log("Updated Majeggstics Member list");
+    console.log(majMemberlist);
+  } catch (error) {
+    console.error("Failed to update Majeggstics Member list:", error);
   }
 }
 
-// loadMajMemberlist();
+loadMajMemberlist();
 
 module.exports = {
-    loadMajMemberlist,
-  };
+  loadMajMemberlist,
+};
