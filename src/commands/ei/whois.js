6598579da56bbd4faf1faa536386b986deda8ce0
Alice <helena.wu4@gmail.com>
2023-06-16 01:13:47 -0700

    added ephemeral option to whois command, per request

--- src/commands/ei/whois.js
+++ src/commands/ei/whois.js
@@ -1,7 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed, EmbedBuilder } = require("discord.js");
 let { data } = require("../../data/maj.json");
-const { convertToLargeNumber, convertToDecimalNumber } = require("../../utils/countDigits.js");
+const {
+  convertToLargeNumber,
+  convertToDecimalNumber,
+} = require("../../utils/countDigits.js");
 
 const fs = require("fs");
 
@@ -13,13 +16,22 @@ module.exports = {
       option
         .setName("query")
         .setDescription(
-          'Partial words are acceptable. e.g., "Jan" for JaneDoe.'
+          'Partial words are acceptable, case insensitive. e.g., "jan" for JaneDoe.'
         )
         .setRequired(true)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("ephemeral")
+        .setDescription("Choose whether the response should be ephemeral.")
     ),
+
+
   async execute(interaction) {
     const searchWord = interaction.options.getString("query");
-    const jsonFile = require("../../data/maj.json"); 
+    const isEphemeral = interaction.options.getBoolean("ephemeral") || false;
+
+    const jsonFile = require("../../data/maj.json");
 
     const matchingEntries = jsonFile.filter((entry) => {
       const { discordName = "", ID = "", IGN = "" } = entry;
@@ -39,7 +51,9 @@ module.exports = {
 
     try {
       if (matchingEntries.length === 0) {
-        await interaction.reply(`No matching entries found for \`${searchWord}\`.`);
+        await interaction.reply(
+          `No matching entries found for \`${searchWord}\`.`
+        );
         return;
       }
 
@@ -48,8 +62,10 @@ module.exports = {
           .map((entry) => `${entry.discordName} - ${entry.IGN}`)
           .join("\n");
 
-          await interaction.reply(`*Multiple entries found, please refine your search.*\n${entriesList}`);
-          return;
+        await interaction.reply(
+          `*Multiple entries found, please refine your search.*\n${entriesList}`
+        );
+        return;
       }
 
       matchingEntries.forEach((entry) => {
@@ -67,22 +83,26 @@ module.exports = {
             {
               name: "Discord Name",
               value: `${discordName}`,
-            },
-            {
-              name: "Farmer Role",
-              value: `${farmerRole}`,
-            },
-            {
-              name: "EB",
-              value: `${EB}`,
-            },
-            {
-              name: "Grade",
-              value: `${grade.toUpperCase()}`,
+              inline: true,
             },
             {
               name: "IGN",
               value: `${IGN}`,
+              inline: true,
+            },
+            {
+              name: "Grade",
+              value: `${grade.toUpperCase()}`,
+              inline: false,
+            },
+            {
+              name: "Farmer Role",
+              value: `${farmerRole}`,
+              inline: true,
+            },
+            {
+              name: "EB",
+              value: `${EB}`,
             }
           )
 
@@ -94,7 +114,7 @@ module.exports = {
           })
           .setTimestamp();
 
-        interaction.reply({ embeds: [embed] });
+        interaction.reply({ embeds: [embed], ephemeral: isEphemeral });
       });
     } catch (error) {
       console.error(error);
@@ -104,124 +124,3 @@ module.exports = {
     }
   },
 };
-
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName("lookup")
-//     .setDescription("Returns data pertaining to the matched user.")
-//     .addStringOption((option) =>
-//       option
-//         .setName("query")
-//         .setDescription(
-//           'Wildcards are acceptable. e.g., looking for JaneDoe with "/Jan*"'
-//         )
-//         .setRequired(true)
-//     ),
-//   async execute(interaction) {
-//     const searchQuery = interaction.options.getString("query");
-//     const jsonFile = require("../../data/maj.json"); // Load the JSON file
-
-//     const matchingEntries = jsonFile.filter((entry) => {
-//       const { discordName, ID, IGN } = entry;
-//       return (
-//         discordName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-//         ID?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-//         IGN?.toLowerCase().includes(searchQuery.toLowerCase())
-//       );
-//     });
-
-//     console.log(`++++++++++++++++++++++++++++++++++++++++++++++`)
-//     console.log(`lookup command results:`)
-//     console.log(`Matching entries: ${matchingEntries}`)
-//     console.log(`searchQuery: ${searchQuery}`)
-//     console.log(`jsonFile: ${jsonFile}`)
-
-//     try {
-//       if (matchingEntries.length === 0) {
-//         await interaction.reply("No matching entries found.");
-//         return;
-//       }
-
-//       matchingEntries.forEach((entry) => {
-//         const { discordName, ID, IGN, farmerRole, grade, EB } = entry;
-
-//         const embed = new MessageEmbed()
-//           .setTitle("Search Results")
-//           .addField("ID", ID)
-//           .addField("Discord Name", discordName)
-//           .addField("Farmer Role", farmerRole)
-//           .addField("EB", EB.toString())
-//           .addField("Grade", grade)
-//           .addField("IGN", IGN);
-
-//         interaction.reply({ embeds: [embed] });
-//       });
-//     } catch (error) {
-//         console.error('Lookup command failed:', error);
-//       interaction.reply({
-//         content: `Something went wrong. ${error.message}`,
-//       });
-//     }
-//   },
-// };
-
-// Command to search the JSON file
-// const searchCommand = new SlashCommandBuilder()
-//   .setName('lookup')
-//   .setDescription('Searches the JSON file for entries')
-//   .addStringOption(option =>
-//     option.setName('query')
-//       .setDescription('The search query')
-//       .setRequired(true));
-
-// client.on('interactionCreate', async (interaction) => {
-//   if (!interaction.isCommand() || interaction.commandName !== 'lookup') return;
-
-//   const searchQuery = interaction.options.getString('query');
-//   const jsonFile = require('src/data/maj.json'); // Load the JSON file
-
-//   const matchingEntries = jsonFile.filter(entry => {
-//     const { discordName, ID, IGN } = entry;
-//     return (
-//       discordName.toLowerCase().includes(searchQuery.toLowerCase()) ||
-//       ID.toLowerCase().includes(searchQuery.toLowerCase()) ||
-//       IGN.toLowerCase().includes(searchQuery.toLowerCase())
-//     );
-//   });
-
-//   if (matchingEntries.length === 0) {
-//     await interaction.reply('No matching entries found.');
-//     return;
-//   }
-
-//   matchingEntries.forEach(entry => {
-//     const {
-//       discordName,
-//       ID,
-//       IGN,
-//       farmerRole,
-//       grade,
-//       EB
-//     } = entry;
-
-//     const embed = new Discord.MessageEmbed()
-//       .setTitle('Search Results')
-//       .addField('ID', ID)
-//       .addField('Discord Name', discordName)
-//       .addField('Farmer Role', farmerRole)
-//       .addField('EB', EB.toString())
-//       .addField('Grade', grade)
-//       .addField('IGN', IGN);
-
-//     await interaction.reply({ embeds: [embed] });
-//   });
-// });
-
-// Register the slash command
-// client.on('ready', async () => {
-//   try {
-//     await client.guilds.cache.get('your_guild_id').commands.set([searchCommand.toJSON()]);
-//   } catch (error) {
-//     console.error('Failed to register slash command:', error);
-//   }
-// });
